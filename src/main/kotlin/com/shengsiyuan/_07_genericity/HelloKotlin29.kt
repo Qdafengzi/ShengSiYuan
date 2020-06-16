629976ef1bada1163933889d390f14e5cad25b20
hemulin <mulin.he@gotechcn.cn>
2020-06-17 00:56:14 +0800

    14

--- src/main/kotlin/com/shengsiyuan/_07_genericity/HelloKotlin29.kt
+++ src/main/kotlin/com/shengsiyuan/_07_genericity/HelloKotlin29.kt
@@ -16,6 +16,27 @@ fun main(args: Array<String>) {
 
     var myGeneric2 = MyGeneric("hello world")//Kotlin类型推断
     println(myGeneric2.variable)
+
+    var myClass = MyClass<String>("abc")
+    myTest(myClass)
+}
+
+//out 关键字 协变
+class MyClass<T>(t: T) {
+    private var t: T
+
+    init {
+        this.t = t
+    }
+
+    fun get(): T {
+        return this.t
+    }
+}
+
+fun myTest(myClass: MyClass<String>) {
+    var myObject: MyClass<String> = myClass
+    println(myObject.get())
 }
 
 // 协变（covariant）和逆变（controversial）
@@ -42,4 +63,17 @@ fun main(args: Array<String>) {
     void copyAll(Collection<Object> to, Collection<String> from) {
     to.addAll(from)
     }
+
+    effective java
+    //协变 用于读取
+    Collection<String> 是Collection<? extends Object> 的子类型 而不是Collection<Object>的子类型
+
+    //逆变 用于写入
+    List<? super String>
+
+    我们如果只从中读取数据，而不往里面写入内容，那么这样的对象叫做生产者
+    如果我们只向里面写入数据，而不从里面读取数据，这样的对象叫消费者
+
+    生产者使用? extends E
+    消费者使用? super E
 */
