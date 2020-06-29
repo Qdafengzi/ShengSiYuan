ae465447da1c0530174cd1a8e8712e0f2cf19809
maike123 <630261105@qq.com>
2020-06-29 18:28:32 +0800

    lesson 15

--- src/main/kotlin/com/shengsiyuan/_07_genericity/HelloKotlin31.kt
+++ src/main/kotlin/com/shengsiyuan/_07_genericity/HelloKotlin31.kt
@@ -1,2 +1,11 @@
 package com.shengsiyuan._07_genericity
 
+/*
+    Kotlin在声明处协变 java在使用处协变
+
+    Kotlin中的out关键字叫做 variance annotation（变量注解），
+    因为他是在类型参数声明处所指定的，因此我们称之为声明处协变（declaration-site variance）
+
+    对于Java来说则是使用处协变（use-site variance），其中类型通配符使得类型协变成为可能
+*/
+
