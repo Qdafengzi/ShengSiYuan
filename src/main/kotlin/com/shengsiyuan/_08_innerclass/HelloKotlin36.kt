d86adfec8968cbcf9224432f1365e99f2e146633
hemulin <mulin.he@gotechcn.cn>
2020-07-02 23:16:19 +0800

    lesson 19

--- src/main/kotlin/com/shengsiyuan/_08_innerclass/HelloKotlin36.kt
+++ src/main/kotlin/com/shengsiyuan/_08_innerclass/HelloKotlin36.kt
@@ -6,6 +6,18 @@ class OuterClass {
 
     class NestedClass {
         fun nestedMethod() = "welcome"
+        //这是不能访问的 因为嵌套类相当于静态内部类 静态内部类不能访问外部类的非静态成员和方法
+        //静态成员隶属于外部类的class对象 而不是外部类，所以会在初始化的时候就进行优先初始化
+        //可以访问同一个外部类当中的别的静态类，因为大家都是静态的 同级别
+        //this@OuterClass.str
+    }
+
+    class NestedClass2{
+        var nestedClass = NestedClass()
+        fun show(){
+            //可以访问同一个外部类当中的别的静态类，因为大家都是静态的 同级别
+            println(nestedClass.nestedMethod())
+        }
     }
 }
 
---
+++ src/main/kotlin/com/shengsiyuan/_08_innerclass/HelloKotlin41.kt
@@ -0,0 +1,2 @@
+package com.shengsiyuan._08_innerclass
+
