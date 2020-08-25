137ea9aaa8801e0d604af175e27d8724f751aaba
maike123 <630261105@qq.com>
2020-08-25 18:20:48 +0800

    lesson 22

--- src/main/kotlin/com/shengsiyuan/_09_delegation/HelloKotlin47.kt
+++ src/main/kotlin/com/shengsiyuan/_09_delegation/HelloKotlin47.kt
@@ -1,1 +1,14 @@
 package com.shengsiyuan._09_delegation
+
+//类委托
+
+interface MyInterface {
+    fun myPrint()
+}
+
+class MyInterfaceImpl(val str: String) : MyInterface {
+    override fun myPrint() {
+        println("welcome $str")
+    }
+
+}
