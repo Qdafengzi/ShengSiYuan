3f6b2139913fdca4805a5ac98ceb27e8ffa3d282
maike123 <630261105@qq.com>
2020-08-26 18:11:25 +0800

    lesson 23 委托

--- src/main/kotlin/com/shengsiyuan/_09_delegation/HelloKotlin48.kt
+++ src/main/kotlin/com/shengsiyuan/_09_delegation/HelloKotlin48.kt
@@ -1,1 +1,23 @@
 package com.shengsiyuan._09_delegation
+
+import kotlin.reflect.KProperty
+
+//属性委托
+
+class MyDelegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String =
+        "$thisRef,your delegate property name is ${property.name}"
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) =
+        println("$thisRef, new value i $value")
+}
+
+class MyPropertyClass {
+    var str: String by MyDelegate()
+}
+
+fun main(args: Array<String>) {
+    val myPropertyClass = MyPropertyClass()
+    myPropertyClass.str = "hello world"
+    println(myPropertyClass.str)
+}
