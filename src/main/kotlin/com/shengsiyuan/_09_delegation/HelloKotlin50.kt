133c35f49b35d5f8229d3290745c5dffb5a38fb9
maike123 <630261105@qq.com>
2020-08-31 18:17:33 +0800

    lesson 23 lazy 非空委托

--- src/main/kotlin/com/shengsiyuan/_09_delegation/HelloKotlin50.kt
+++ src/main/kotlin/com/shengsiyuan/_09_delegation/HelloKotlin50.kt
@@ -3,7 +3,7 @@ package com.shengsiyuan._09_delegation
 import kotlin.properties.Delegates
 
 // 非空属性
-
+// notNUll 适用于那些无法再初始化阶段无法确认属性值的场合
 class MyPerson {
     var address: String by Delegates.notNull<String>()
 }
