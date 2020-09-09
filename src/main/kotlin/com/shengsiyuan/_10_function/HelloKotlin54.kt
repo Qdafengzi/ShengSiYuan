c79d68ac83e1998dadaf9960899c0820ef02fd24
maike123 <630261105@qq.com>
2020-09-09 18:34:57 +0800

    lesson 28 可变参数

--- src/main/kotlin/com/shengsiyuan/_10_function/HelloKotlin54.kt
+++ src/main/kotlin/com/shengsiyuan/_10_function/HelloKotlin54.kt
@@ -24,3 +24,22 @@ fun myPrint3(a: Int, b: Int): Int = a + b
 fun myPrint4(a: Int, b: Int): Int {
     return a + b
 }
+
+/*
+    一个方法中，只允许一个参数为vararg，通常作为最后一个参数。
+    如果vararg不是最后一个参数，那么其后的参数就需要通过具名参数形式传递。
+    如果其后的参数是函数类型，那么还可以通过在圆括号外传递lambda表达式来实现。
+ */
+
+fun <T> convert2List(vararg elements: T): List<T> {
+    val result = ArrayList<T>()
+    elements.forEach { result.add(it) }
+    return result
+}
+
+fun main(args: Array<String>) {
+    println(convert2List("hello", "world"))
+
+    val elements = arrayOf("welcome", "bye", "hello")
+    println(convert2List(*elements))
+}
